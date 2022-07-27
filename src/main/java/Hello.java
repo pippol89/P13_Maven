e4a7d6ff97639f629a769f1c9b63eb99f05d3b34
pippol89 <seedorf10@rambler.ru>
2022-07-27 09:02:01 +0300

    Lesson 9_1 (Maven)

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        List<Integer> list = new ArrayList<>();
+    }
+}
---
+++ src/test/java/QTest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QTest {
+    @Test
+    public void testOne() {
+        Assert.assertEquals(5, 5);
+    }
+}
